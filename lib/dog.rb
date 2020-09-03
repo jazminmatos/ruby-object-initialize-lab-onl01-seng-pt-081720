dcb7cc938e62ca5e55f8d18879e427ba1ee38c54
Jazmin Matos <jmatos1993@gmail.com>
2020-09-03 01:09:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  def name=(name) #setter method 
+    @name=name
+  end
+  
+  def name #getter method
+    @name
+  end
+  
+  def breed=(breed) #setter method 
+    @breed=breed
+  end
+  
+  def breed #getter method
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+  
+  def name=(name) #setter method 
+    @name=name
+  end
+  
+  def name #getter method
+    @name
+  end
+end
